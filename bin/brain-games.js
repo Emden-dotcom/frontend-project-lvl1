98cb8198a4feed3f14fdfe356c0c772a1c0419f9
Emden-dotcom <alex.kyrychenko@gmail.com>
2021-10-03 23:34:02 +0200

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+import { userName } from '../src/cli.js'
 
 
-Welcome to the Brain Games!
+
+console.log('Hello ' + userName + '!');
+
+
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,11 @@
 
-
 import readlineSync from 'readline-sync';
 
 
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi, ' + userName + '!');
+console.log('Welcome to the Brain Games!');
+export const userName = readlineSync.question('May I have your name? ');
+
+
+
+
+
