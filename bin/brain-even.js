54339b216c53a54917665de5865b63b558ad6659
Emden-dotcom <alex.kyrychenko@gmail.com>
2021-10-08 13:48:13 +0200

    bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,4 +5,3 @@ console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello ${userName}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
-
