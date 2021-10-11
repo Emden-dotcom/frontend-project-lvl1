57d1b8be53f9390bda9075cace627af69f220436
Emden-dotcom <alex.kyrychenko@gmail.com>
2021-10-11 14:43:35 +0200

    package-lock.json

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,4 +4,27 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello ${userName}`);
-console.log('Answer "yes" if the number is even, otherwise answer "no"');
+console.log('Answer "yes" if the number is even, otherwise answer "no",');
+
+for (let i = 1; i < 4; i += 1) {
+  const randomNumber = Math.floor(Math.random() * 100);
+  console.log('Question:', randomNumber);
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  if (randomNumber % 2 === 0) {
+    if (playerAnswer === 'yes') {
+      console.log('Correct!');
+    } else {
+      console.log(`"no" is wrong answer ;(. Correct answer was "yes".\nLet's try again, ${userName}`);
+    }
+  } else if (randomNumber % 2 !== 0) {
+    if (playerAnswer === 'no') {
+      console.log('Correct!');
+    } else {
+      console.log(`"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}`);
+    }
+  } else {
+    console.log(`"Wrong answer ;(.\nLet's try again, ${userName}`);
+  }
+}
+console.log(`Congratulations, ${userName}!`);
