e9a7e9610e37ced0d65b4cb6c7766cbec7aa6cd4
Emden-dotcom <alex.kyrychenko@gmail.com>
2021-10-12 19:40:58 +0200

    brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { userName } from '../src/index.js';
+
+console.log('Welcome to the Brain Games!');
+console.log(`Hello ${userName}`);
+console.log('Answer "yes" if the number is even, otherwise answer "no",');
