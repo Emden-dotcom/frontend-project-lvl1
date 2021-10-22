da0f26a7345014ad3a71c7cb0c17f0931c336f3c
Emden-dotcom <alex.kyrychenko@gmail.com>
2021-10-22 22:15:14 +0200

    bin/brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -45,7 +45,7 @@ const brainCalculator = () => {
     if (playerAnswer === commonDivisor.toString()) {
       console.log('Correct!');
     } else {
-      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${commonDivisor}".\nLet's try again, ${userName}!`);
+      console.log(`Your answer: "${playerAnswer}" is wrong answer ;(. Correct answer was "${commonDivisor}".\nLet's try again, ${userName}!`);
       return;
     }
   }
